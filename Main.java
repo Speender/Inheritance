90e1bc549e1ba9bd61da057f0e23102ffdca257a
Theo <143622653+Speender@users.noreply.github.com>
2024-09-15 21:39:23 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,19 +5,20 @@ public class Main {
         Motorcycle motor = new Motorcycle("Kawasaki Ninja", 165, 10, "Black & Gold", 400, 180);
         Car car = new Car("Tesla Cyber Truck", 200, 0, "Black", "Electric", "Automatic");
         Bus bus = new Bus("Yutong Ceres Bus", 90, 250, "Yellow", "Air-Conditioned", "Cebu - Bantayan Island");
-        boolean doorOpen = false;
-        boolean wiperOn = false;
+        Random rand = new Random();
+        boolean doorOpen = rand.nextBoolean();
+        boolean wiperOn = rand.nextBoolean();
         
+        motor.displayDetails();
         motor.banking();
         motor.wheelie();
-        motor.displayDetails();
 
+        car.displayDetails();
         car.toggleDoor(doorOpen);
         car.toggleWiper(wiperOn);
-        car.displayDetails();
 
+        bus.displayDetails();
         bus.pickUpPassenger();
         bus.destination();
-        bus.displayDetails();
     }
 }
